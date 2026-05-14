3b7d66b6b8c01a2694ef16305242d501ab498d20
ChagasMarcos <antunesm@hotmai.com>
2026-05-14 19:49:16 -0300

    Routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ShopComponent } from './features/shop/shop.component';
+import { ProducDetailsComponent } from './features/shop/produc-details/produc-details.component';
+import { LoginComponent } from './features/login/login.component';
+import { RegisterComponent } from './features/register/register.component';
+import { ContactComponent } from './features/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'shop/:id', component: ProducDetailsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
